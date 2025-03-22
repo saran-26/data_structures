6e44d49d8df75a0c2405c5e8dd6ae7c3aa4b1b0a
saran-26 <sarandevaraj6@gmail.com>
2025-03-22 11:33:10 +0530

    Create linkedlist.java

---
+++ linkedlist.java
@@ -0,0 +1,35 @@
+package link;
+import java .util.*;
+class Node{
+	int data;
+	Node next;
+	
+}
+class main
+{
+	public static void insert(Node head)
+	{
+		Scanner sc = new Scanner(System.in);
+		int pos=sc.nextInt();
+		int c=0;
+		Node p=head;
+		while(c<(pos-1)&&p.next!=null)
+		{
+			
+		}
+
+
+
+				
+		
+
+				
+	}
+}
+public class linkedlist1 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
